d402ed7ed63f973a67cfd1641617bd841f312552
Alex Gonçalves <62771511+alexsilvagoncalves89@users.noreply.github.com>
2026-05-30 21:20:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ except Exception as e:
 # 3. MENU DE NAVEGAÇÃO LATERAL (Sidebar)
 # ==============================================================================
 st.sidebar.title("🏥 Portal Médico")
-st.sidebar.markdown("Sistema de Inteligência Artificial de apoio à avaliação clínica")
+st.sidebar.markdown("Sistema de Suporte à Decisão Clínica")
 st.sidebar.divider()
 
 opcao_menu = st.sidebar.radio(
